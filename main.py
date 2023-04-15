ec86cef717b3e8224a8cac90e3ad639710a8d3c3
siriiuss <ahmetdur10@gmail.com>
2023-04-15 13:12:10 +0300

    Optimized

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from fastapi import FastAPI, Response, HTTPException
-from fastapi.responses import RedirectResponse, HTMLResponse
+from fastapi import FastAPI, Response
+from fastapi.responses import RedirectResponse
 import json
-import websockets
-
-from time import sleep
 
 import requests
 from bs4 import BeautifulSoup
@@ -17,7 +14,7 @@ def tff_data(data_type, url="https://www.tff.org/default.aspx?pageID=198"):
 
     table = soup.find("table", {"class": "s-table"})
 
-    teams = []  # Tüm takımları saklayacak bir liste oluşturun
+    teams = []
 
     rows = table.findAll("tr")[1:]
 
@@ -37,31 +34,28 @@ def tff_data(data_type, url="https://www.tff.org/default.aspx?pageID=198"):
         }
         teams.append(team_data)
 
+    with open("table.json", "w", encoding="utf-8") as f:
+        json.dump(teams, f, ensure_ascii=False, indent=4)
+    f.close()
 
+    with open('table.json', 'r', encoding="utf-8") as json_file:
+        json_object = json.load(json_file)
 
-    return teams
+    if data_type == "json_file":
+        return json_object
+    elif data_type == "live":
+        return teams
 
 
 @app.get("/")
 async def root():
-    return RedirectResponse("/live")
+    json_str = json.dumps(tff_data("live"), indent = 4, default = str, ensure_ascii=False)
+    return Response(content=json_str, media_type='application/json'
 
 @app.get("/json")
 async def json_type():
-    html = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <title>Unavaible</title>
-     <meta http-equiv="refresh" content="10;URL='/live'" />
-    </head>
-    <body style="background-color: #121212;">
-    <p style="color: white; font-size: 14px;">The JSON type is unavailable due to system capacity issues. You will be redirected to Live in 10 seconds. For more detail <a href="" style="color: white;"> check documentation</a>
-    </p>
-    </body>
-    </html>
-    """
-    return HTMLResponse(html)
+    json_str = json.dumps(tff_data("json_file"), indent = 4, default = str, ensure_ascii=False)
+    return Response(content=json_str, media_type='application/json')
 
 @app.get("/live")
 async def live():
